220a5e3ddfad4c94e8a905f6ef94be7e85717c1b
Linda Kovacs <lindaprovenza@yahoo.com>
2020-05-25 14:48:16 -0400

    feat/initial-cards refactor HTML code using templating and JS for cards

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,30 @@
+const initialCards = [
+    {
+        name: "Yosemite Valley",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+        name: "Lake Louise",
+        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+        name: "Latemar",
+        link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+        name: "Vanois National Park",
+        link: "https://code.s3.yandex.net/web-code/vanois.jpg"
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+];
+
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.form__reset-button');
 const form = document.querySelector('.form');
@@ -22,4 +49,29 @@ formInput.addEventListener('submit', (e) => {
     subtitleValue.textContent = subtitleInput.value;
 
     toggleForm();
+});
+
+const cardTemplate = document.querySelector('.card-template').content.querySelector('.card');
+const listWrapper = document.querySelector('.cards__grid');
+
+
+function createCard(card) {
+    const cardElement = cardTemplate.cloneNode(true);
+
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const cardLikeButton = cardElement.querySelector('.card__like-button');
+   
+
+    cardTitle.textContent = card.name;
+    cardImage.style.backgroundImage = 'url(' + card.link +')';
+    
+    cardLikeButton.addEventListener('click', () => {
+        // changeLikeColor()
+    });
+    return cardElement;
+}
+
+initialCards.forEach((card) => {
+    listWrapper.prepend(createCard(card));
 });
