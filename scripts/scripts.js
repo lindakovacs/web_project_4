9ee5e9c003766993b2c95474c6ad8be14d7a1db9
Linda Kovacs <lindaprovenza@yahoo.com>
2020-05-31 22:07:51 -0400

    feat/initial-cards add functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -25,53 +25,110 @@ const initialCards = [
     }
 ];
 
+const editFormModal = document.querySelector('.form__edit');
+const addFormModal = document.querySelector('.form__add');
+const openImageFormModal = document.querySelector('.form__add-image');
+let formImageModal = document.querySelector('.form__image');
+let formImageTitle = document.querySelector('.form__image-title');
+
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.form__reset-button');
-const form = document.querySelector('.form');
-const saveButton = editButton.querySelector('.form__submit-button');
+const editCloseIcon = editFormModal.querySelector('.form__reset-button');
+const addCloseIcon = addFormModal.querySelector('.form__reset-button');
+const addButton = document.querySelector('.profile__add-button');
+const imageCloseIcon = openImageFormModal.querySelector('.form__reset-button');
 
-const formInput = document.querySelector('.form__container');
-const titleInput = formInput.querySelector('.form__title');
-const subtitleInput = formInput.querySelector('.form__subtitle');
+const nameInput = document.querySelector('.form__title');
+const jobInput = document.querySelector('.form__subtitle');
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__subtitle');
 
-const titleValue = document.querySelector('.profile__title');
-const subtitleValue = document.querySelector('.profile__subtitle');
+const titleInput = document.querySelector('.form__card-title');
+const linkInput = document.querySelector('.form__image-link');
 
-const toggleForm = () => form.classList.toggle('form_visible');
+const listWrapper = document.querySelector('.cards__grid');
+const templateCard = document.querySelector('.card-template').content.querySelector('.card');
 
-editButton.addEventListener('click', toggleForm);
-closeButton.addEventListener('click', toggleForm);
 
-formInput.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    titleValue.textContent = titleInput.value;
-    subtitleValue.textContent = subtitleInput.value;
-
-    toggleForm();
+addButton.addEventListener('click', () => {
+  toggleForm(addFormModal);
+});
+addCloseIcon.addEventListener('click', () => {
+  toggleForm(addFormModal);
 });
 
-const cardTemplate = document.querySelector('.card-template').content.querySelector('.card');
-const listWrapper = document.querySelector('.cards__grid');
+editButton.addEventListener('click', () => {
+  toggleForm(editFormModal);
+});
+editCloseIcon.addEventListener('click', () => {
+  toggleForm(editFormModal);
+});
 
+imageCloseIcon.addEventListener('click', () => {
+  toggleForm(openImageFormModal);
+});
+
+editFormModal.addEventListener('submit', editFormSubmitHandler);
+addFormModal.addEventListener('submit', addFormSubmitHandler);
+
+function toggleForm (card) {
+  card.classList.toggle('form_visible');
+}
+
+function editFormSubmitHandler (e) {
+    e.preventDefault();
+
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+
+    toggleForm(editFormModal);
+}
+
+function addFormSubmitHandler (e) {
+  e.preventDefault();
+
+  const newCard =
+    {
+        name: titleInput.value,
+        link: linkInput.value
+    };
+  renderCard(newCard);
+  toggleForm(addFormModal);
+}
+
+function renderCard(card) {
+  listWrapper.prepend(createCard(card));
+}
 
 function createCard(card) {
-    const cardElement = cardTemplate.cloneNode(true);
+  const cardModal = templateCard.cloneNode(true);
+  const imageModal = cardModal.querySelector('.card__image');
+  const titleModal = cardModal.querySelector('.card__title');
+  const deleteButtonModal = cardModal.querySelector('.card__delete-button');
+  const likeModal = cardModal.querySelector('.card__like-button');
 
-    const cardImage = cardElement.querySelector('.card__image');
-    const cardTitle = cardElement.querySelector('.card__title');
-    const cardLikeButton = cardElement.querySelector('.card__like-button');
-   
+  imageModal.style.backgroundImage = `url('${card.link}')`;
+  titleModal.textContent = card.name;
 
-    cardTitle.textContent = card.name;
-    cardImage.style.backgroundImage = 'url(' + card.link +')';
-    
-    cardLikeButton.addEventListener('click', () => {
-        // changeLikeColor()
-    });
-    return cardElement;
+  deleteButtonModal.addEventListener('click', () => {
+    const listItem = deleteButtonModal.closest(".card");
+    listItem.remove();
+  });
+
+  likeModal.addEventListener('click', (e) => {
+    e.target.classList.toggle('card__like_active');
+  });
+
+  imageModal.addEventListener('click', () => {
+    formImageModal.src = `${card.link}`;
+    formImageModal.alt = `${card.name.replace(/\s+/g, '-').toLowerCase()}`;
+    formImageTitle.textContent = card.name;
+    toggleForm(openImageFormModal);
+  });
+
+  return cardModal;
 }
 
 initialCards.forEach((card) => {
-    listWrapper.prepend(createCard(card));
+  renderCard(card);
 });
+
